ea7ec057ae0508aacdda2b5440539a26b994ed17
Luke Armstrong <Ltarmstrong96@gmail.com>
2024-10-16 10:25:30 -0700

    Adding spacebar to increment

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Count from './Count';
 import ButtonContainer from './ButtonContainer';
 import ResetButton from './ResetButton';
@@ -6,13 +6,33 @@ import Title from './Title';
 
 export default function Card() {
   const [count, setCount] = useState(0);
+  const locked = count === 5 ? true : false;
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      if (event.code === 'Space') {
+        const newCount = count + 1;
+        if (newCount > 5) {
+          setCount(5);
+          return;
+        }
+        setCount(newCount);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, [count]);
 
   return (
-    <div className='card'>
-      <Title />
+    <div className={`card ${locked ? 'card--limit' : ''}`}>
+      <Title locked={locked} />
       <Count count={count} />
       <ResetButton setCount={setCount} />
-      <ButtonContainer setCount={setCount} />
+      <ButtonContainer setCount={setCount} locked={locked} />
     </div>
   );
 }
