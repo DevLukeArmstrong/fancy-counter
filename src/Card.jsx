3e4bbbdade24b7b45feccd7e0850ba14e4634383
Luke Armstrong <Ltarmstrong96@gmail.com>
2024-10-16 09:11:01 -0700

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Count from './Count';
+import ButtonContainer from './ButtonContainer';
+import ResetButton from './ResetButton';
+import Title from './Title';
+
+export default function Card() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='card'>
+      <Title />
+      <Count count={count} />
+      <ResetButton setCount={setCount} />
+      <ButtonContainer setCount={setCount} />
+    </div>
+  );
+}
